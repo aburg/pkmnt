a3bbde75f53044fa77f8daef8a3075db2bd2f626
Andreas Burg <andreas.burg@gmx.net>
2024-03-21 23:13:42 +0100

    render top level device list

--- src/ui.rs
+++ src/ui.rs
@@ -1,7 +1,7 @@
 use ratatui::{
     layout::Alignment,
-    style::{Color, Style},
-    widgets::{Block, BorderType, Paragraph},
+    style::{Color, Modifier, Style},
+    widgets::{Block, BorderType, Borders, List, Paragraph},
     Frame,
 };
 
@@ -13,22 +13,30 @@ pub fn render(app: &mut App, frame: &mut Frame) {
     // See the following resources:
     // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
     // - https://github.com/ratatui-org/ratatui/tree/master/examples
+    // frame.render_widget(
+    //     Paragraph::new(format!(
+    //         "This is a tui template.\n\
+    //             Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
+    //             Press left and right to increment and decrement the counter respectively.\n\
+    //             Counter: {}",
+    //         app.counter
+    //     ))
+    //     .block(
+    //         Block::bordered()
+    //             .title("Template")
+    //             .title_alignment(Alignment::Center)
+    //             .border_type(BorderType::Rounded),
+    //     )
+    //     .style(Style::default().fg(Color::Cyan).bg(Color::Black)),
+    //     frame.size(),
+    // )
+
+    let devices = ["dev1", "dev2", "dev3"];
     frame.render_widget(
-        Paragraph::new(format!(
-            "This is a tui template.\n\
-                Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
-                Press left and right to increment and decrement the counter respectively.\n\
-                Counter: {}",
-            app.counter
-        ))
-        .block(
-            Block::bordered()
-                .title("Template")
-                .title_alignment(Alignment::Center)
-                .border_type(BorderType::Rounded),
-        )
-        .style(Style::default().fg(Color::Cyan).bg(Color::Black))
-        .centered(),
+        List::new(devices)
+            .block(Block::default().borders(Borders::ALL))
+            .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
+            .highlight_symbol(">"),
         frame.size(),
     )
 }
